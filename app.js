70be52c8f600f4f13e0a51968660946a78e6a2b4
MATHEUS ZANONI BARBOSA <23111754@CFP127ALUNO.LOCAL>
2023-09-13 09:53:44 -0300

    aula3 finalizada

---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const botaoSomar=document.getElementById('somar')
+const botaoIdentificar=document.getElementById('identificar')
+const botaoParOuImpar=document.getElementById('botao-j')
+const botaoDiferenca=document.getElementById('botao-a')
+const botaoSomaCem=document.getElementById('botao-n')
+const botaoNotas=document.getElementById('botao-d')
+
+function somar(){
+      const numero1=Number(document.getElementById('numero1').value)
+      const numero2=Number(document.getElementById('numero2').value)
+      const resultado=document.getElementById('resultado')
+
+      const total=numero1+numero2
+      resultado.textContent=total
+}
+
+ 
+function parOuImpar(){
+    const valor=Number(document.getElementById('numero-j').value)
+    const resultado=document.getElementById('resultado-j')
+    if(valor%2==0){
+        resultado.textContent='PAR'
+    }else{
+        resultado.textContent='ÍMPAR'
+    }
+}
+
+function diferenca(){
+    const valor1=Number(document.getElementById('numero-a1').value)
+    const valor2=Number(document.getElementById('numero-a2').value)
+    const resultado=document.getElementById('resultado-a')
+    if(valor1>valor2){
+        const diferenca=valor1-valor2
+        resultado.textContent=diferenca
+    }else{
+        const diferenca=valor2-valor1
+        resultado.textContent=diferenca
+    }
+}
+
+function notas(){
+    const valor1=Number(document.getElementById('numero-d1').value)
+    const valor2=Number(document.getElementById('numero-d2').value)
+    const valor3=Number(document.getElementById('numero-d3').value)
+    const valor4=Number(document.getElementById('numero-d4').value)
+    const resultado=document.getElementById('resultado-d')
+    const media=(Number(valor1)+Number(valor2)+Number(valor3)+Number(valor4))/4
+    if(media>=7){
+        alert("APROVADO! MÉDIA: "+media)
+        resultado.textContent=media
+    }else{
+        const valor5=prompt("MÉDIA INSUFICIENTE: "+media+". INSIRA SUA NOTA DE EXAME.")
+        const media2=(media+Number(valor5))/2
+        if(media2>=5){
+            alert("APROVADO EM EXAME! MÉDIA: "+media2)
+            resultado.textContent=media2
+        }else{
+            alert("REPROVADO! MÉDIA: "+media2)
+            resultado.textContent=media2
+        }
+    }
+}
+
+botaoSomar.addEventListener('click',somar)
+botaoParOuImpar.addEventListener('click',parOuImpar)
+botaoDiferenca.addEventListener('click',diferenca)
+botaoNotas.addEventListener('click',notas)
